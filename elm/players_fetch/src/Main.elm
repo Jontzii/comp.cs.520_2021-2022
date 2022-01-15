1861c10fce40d6f0e836d694b2723e1e56cd8ee0
Joonas Hiltunen <joonas@jontzi.com>
2022-01-15 22:51:55 +0200

    Most of 2.3 done

--- elm/players_fetch/src/Main.elm
+++ elm/players_fetch/src/Main.elm
@@ -2,6 +2,9 @@
 -- Update the id from the fetched players
 -- Add player to the end of the list
 
+-- TODO:
+-- Set isActive from fetched players
+-- Set correct player id for new players
 
 module Main exposing (..)
 
@@ -75,27 +78,99 @@ init _ =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
+    let
+        players = model.players
+        newPlayer = model.newPlayer
+    in
     case msg of
         SetName word ->
-            ( model, Cmd.none )
+            ( { model | newPlayer =
+                { id=newPlayer.id
+                , name=word
+                , isActive=newPlayer.isActive
+                }
+            }, Cmd.none )
 
         AddPlayer ->
-            ( model, Cmd.none )
+            ( { model
+            | players = List.reverse (newPlayer :: (List.reverse players))
+            , newPlayer = initPlayer (newPlayer.id + 1)
+            }, Cmd.none )
 
         DeletePlayer id ->
-            ( model, Cmd.none )
+            ( { model | players = List.filter (\x -> x.id /= id) players }, Cmd.none )
 
         ModifyPlayer id status ->
-            ( model, Cmd.none )
+            ( { model | players = List.map (\x -> if x.id == id then { x | isActive = status } else x) players }, Cmd.none )
 
         FetchPlayers data ->
-            ( model, Cmd.none )
+            case data of
+                Ok fetchedPlayers ->
+                    (
+                        let
+                            _ = Debug.log "Data" fetchedPlayers
+                        in
+                        
+                        { model
+                        | reqStatus = ""
+                        , players = fetchedPlayers
+                        }
+                        , Cmd.none
+                    )
+                Err _ ->
+                    (
+                        { model | reqStatus = "An error has occurred!!!" }
+                        , Cmd.none
+                    )
 
+
+-- Render the player form
+renderForm : String -> (String -> Msg) -> Msg -> Html Msg
+renderForm name setName addPlayer =
+    Html.form [ id "submit-player", onSubmit addPlayer ]
+        [ input
+            [ id "input-player"
+            , type_ "text"
+            , placeholder "player name"
+            , value name
+            , onInput setName
+            ] []
+        , button [ id "btn-add" , type_ "submit" ] [ text "Add" ]
+        ]
+
+-- Render single player
+renderPlayer : Player -> (Int -> Msg) -> (Int -> Bool -> Msg) -> Html Msg
+renderPlayer player delete modify =
+    li [ id (String.append "player-" (String.fromInt player.id)) ]
+        [ div [ class "player-name" ] [ text player.name ]
+        , div []
+            [ input
+                [ type_ "checkbox"
+                , class "player-status"
+                , onCheck (\checkValue -> (modify player.id checkValue))
+                ] []
+            , text "active"
+            ]
+        , button
+            [ class "btn-delete"
+            , onClick (delete player.id)
+            ] [ text "Delete" ]
+        ]
 
 view : Model -> Html Msg
 view model =
-    p [] [ text "Elm Exercise: Players Fetch" ]
-
+    let
+        players = model.players
+        reqStatus = model.reqStatus
+    in
+    div []
+        [ h3 [] [ text "Add player" ]
+        , renderForm model.newPlayer.name SetName AddPlayer
+        , h3 [] [ text "Players list" ]
+        , ol [ id "players-list" ]
+            (List.map (\x -> (renderPlayer x DeletePlayer ModifyPlayer)) players)
+        , div [ id "request-status"] [ text reqStatus ]
+        ]
 
 main : Program () Model Msg
 main =
