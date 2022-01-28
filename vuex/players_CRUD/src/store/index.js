6fe254d2587dbf420d1ea9802f340de4f5130b67
Joonas Hiltunen <joonas@jontzi.com>
2022-01-28 20:46:56 +0200

    3.5 done

--- vuex/players_CRUD/src/store/index.js
+++ vuex/players_CRUD/src/store/index.js
@@ -1,55 +1,56 @@
-import { createStore } from 'vuex'
-import Constant from '../Constant';
+import { createStore } from "vuex";
+import Constant from "../Constant";
 
 export default createStore({
   state: {
-    players: [ 
-        { name: 'Kurri' },
-        { name: 'Gretzky' }
-      ],
-    notifications: []
+    players: [{ name: "Kurri" }, { name: "Gretzky" }],
+    notifications: [],
   },
   mutations: {
-    addPlayer(state, name){
-      //TODO - mutate the state
+    addPlayer(state, name) {
+      const newPlayer = { name };
+      state.players.push(newPlayer);
     },
-    removeIndex(state, index){
-      //TODO - mutate the state
+    removeIndex(state, index) {
+      state.players.splice(index, 1);
     },
-    removeLast(state){
-      //TODO - mutate the state
+    removeLast(state) {
+      const amountOfPlayers = state.players.length - 1;
+      state.players.splice(amountOfPlayers, 1);
     },
     addNotification(state, status) {
-      //TODO - mutate the state
+      state.notifications.push(status);
     },
     removeAllNotifications(state) {
-      //TODO - mutate the state
-    }
+      state.notifications = [];
+    },
   },
 
   actions: {
-    [Constant.REMOVE_PLAYER]({ commit,  } , ind) {
-      commit( 'removeIndex', ind );
+    [Constant.REMOVE_PLAYER]({ commit }, ind) {
+      commit("removeIndex", ind);
     },
-    [Constant.REMOVE_LAST]({ commit,  } ) {
-      commit( 'removeLast' );
+    [Constant.REMOVE_LAST]({ commit }) {
+      commit("removeLast");
     },
-    [Constant.REMOVE_DELAY]({ commit,  } ) {
-        commit('addNotification', "The last item will be removed with delay")
-        //TODO - remove with delay, remove also the notification
+    [Constant.REMOVE_DELAY]({ commit }) {
+      commit("addNotification", "The last item will be removed with delay");
+      setTimeout(() => {
+        commit("removeLast");
+        commit("removeAllNotifications");
+      }, 2000);
     },
-    [Constant.ADD_PLAYER]({ commit,  } , e) {
-      const form = e.target
-      const inputElem =form.querySelector("#input_player") 
-      const inputVal = inputElem.value
-      commit('addPlayer', inputVal)
+    [Constant.ADD_PLAYER]({ commit }, e) {
+      const form = e.target;
+      const inputElem = form.querySelector("#input_player");
+      const inputVal = inputElem.value;
+      commit("addPlayer", inputVal);
     },
   },
   getters: {
-    players: state => state.players,
-    notifications: state => {
-      return state.notifications
+    players: (state) => state.players,
+    notifications: (state) => {
+      return state.notifications;
     },
-
-  }
-})
+  },
+});
