00857cae7b94fce5389b5b2fa5c89e40391f41e3
Joonas Hiltunen <joonas@jontzi.com>
2022-02-14 13:24:51 +0200

    5.2 Done

--- redux/pure_redux/reducer.js
+++ redux/pure_redux/reducer.js
@@ -1,6 +1,48 @@
-import * as actions from './actionTypes.js';
+import * as actions from "./actionTypes.js";
 
 export default (state = [], action) => {
-  // TODO: Implement this function
-  throw new Error('Not Implemented');
+  try {
+    switch (action.type) {
+      case actions.ADD_PLAYER:
+        // Find the largest id from store
+        let nextId = 1;
+
+        if (state.length > 0) {
+          const largestId = state.reduce((prev, curr) => {
+            if (prev.id >= curr.id) return prev;
+            else return curr;
+          });
+
+          nextId = largestId.id + 1;
+        }
+
+        state.push({
+          id: nextId,
+          name: action.payload.name,
+          isActive: action.payload.isActive,
+        });
+
+        return state;
+      case actions.REMOVE_PLAYER:
+        const rm_index = state.findIndex((val) => val.id === action.payload.id);
+
+        if (rm_index === -1) return state;
+
+        state.splice(rm_index, 1);
+        return state;
+      case actions.TOGGLE_PLAYER_STATUS:
+        const toggle_index = state.findIndex(
+          (val) => val.id === action.payload.id
+        );
+
+        if (toggle_index === -1) return state;
+
+        state[toggle_index].isActive = !state[toggle_index].isActive;
+        return state;
+      default:
+        return state;
+    }
+  } catch (e) {
+    return state;
+  }
 };
