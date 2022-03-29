e468158fbe591dc05cb8c3071031bd207891905e
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-29 21:00:00 +0300

    Implement high-order-components
    Finder implemented

--- assignment/frontend/src/components/Auth.jsx
+++ assignment/frontend/src/components/Auth.jsx
@@ -1,11 +1,41 @@
 /** @format */
 
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { Outlet } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { Outlet } from "react-router-dom";
 
-const Auth = ({ authRoles }) => {};
+/**
+ * A resuable higher-order-component that checks the user has the required role in order to access the given route
+ * @param {Array} authRoles An array of roles that are authorized to access this path
+ * @returns Child when authorized, null otherwise
+ */
+const Auth = ({ authRoles }) => {
+  const role = useSelector((state) => state.auth).role;
+  const [isAuthrorized, setAuthorized] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!role | (role === "guest") && !authRoles.includes("guest")) {
+      setAuthorized(false);
+      navigate("/login");
+    } else if (role && !authRoles.includes(role)) {
+      setAuthorized(false);
+      navigate("/");
+    } else if (role && authRoles.includes(role)) {
+      setAuthorized(true);
+    }
+  }, [role, authRoles, navigate]);
+
+  if (isAuthrorized) {
+    return (
+      <div data-testid="auth-success-component">
+        <Outlet />
+      </div>
+    );
+  }
+
+  return null;
+};
 
 export default Auth;
--- assignment/frontend/src/components/Finder.jsx
+++ assignment/frontend/src/components/Finder.jsx
@@ -1,10 +1,53 @@
 /** @format */
 
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useParams } from "react-router-dom";
 
-const Finder = ({ type, findHandler }) => {};
+/**
+ * Finds a single item based on the prop type and the id of the item
+ * @param {*} param0
+ */
+const Finder = ({ type, findHandler }) => {
+  const [itemFound, setFound] = useState(false);
+  const { productId, orderId, userId } = useParams();
+
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+  const orders = useSelector((state) => state.orders);
+  const users = useSelector((state) => state.users);
+
+  useEffect(() => {
+    switch (type) {
+      case "product":
+        setFound(products.some((val) => val.id === productId));
+        break;
+
+      case "order":
+        setFound(orders.some((val) => val.id === orderId));
+        break;
+
+      case "user":
+        setFound(users.some((val) => val.id === userId));
+        break;
+
+      default:
+        break;
+    }
+  }, [type, productId, orderId, userId, orders, products, users]);
+
+  if (!itemFound) {
+    dispatch(findHandler());
+    return (
+      <div data-testid={`no-${type}-found-component`}>{type} not found.</div>
+    );
+  }
+
+  return (
+    <div data-testid={`${type}-found-component`}>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Finder;
