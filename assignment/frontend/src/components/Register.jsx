4f55e9a2023d4f86d8f899a7fd6be9eb13122847
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-31 00:17:31 +0300

    Implement Register

--- assignment/frontend/src/components/Register.jsx
+++ assignment/frontend/src/components/Register.jsx
@@ -1,9 +1,73 @@
 /** @format */
 
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { register } from '../redux/actionCreators/authActions';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { register } from "../redux/actionCreators/authActions";
 
-const Register = () => {};
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const dispatch = useDispatch();
+
+  /**
+   * Registers a new user
+   * @param {*} e Event
+   */
+  const registerUser = (e) => {
+    e.preventDefault();
+    dispatch(register({ name, email, password, passwordConfirmation }));
+  };
+
+  return (
+    <div data-testid="register-component">
+      <form data-testid="register-form" onSubmit={registerUser}>
+        <label>
+          Name:
+          <input
+            data-testid="name-input"
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            data-testid="email-input"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            data-testid="password-input"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          Password confirmation:
+          <input
+            data-testid="passwordConfirmation-input"
+            type="password"
+            required
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+        </label>
+        <input data-testid="register-button" type="submit" value="Register" />
+      </form>
+    </div>
+  );
+};
 
 export default Register;
