df4478919c09ad919508dd56b715e436c4564619
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-30 20:52:07 +0300

    Cart and CartItem implemented

--- assignment/frontend/src/components/Cart.jsx
+++ assignment/frontend/src/components/Cart.jsx
@@ -1,11 +1,57 @@
 /** @format */
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { createNotification } from '../redux/actionCreators/notificationsActions';
-import { addOrder } from '../redux/actionCreators/ordersActions';
-import CartItem from './CartItem';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { createNotification } from "../redux/actionCreators/notificationsActions";
+import { addOrder } from "../redux/actionCreators/ordersActions";
+import CartItem from "./CartItem";
 
-const Cart = () => {};
+/**
+ * @component
+ * @returns
+ */
+const Cart = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const cartState = useSelector((state) => state.cart);
+  const role = useSelector((state) => state.auth).role;
+
+  /**
+   * Handle order
+   * @param {*} e Event
+   */
+  const orderClick = (e) => {
+    e.preventDefault();
+
+    if (!role || role === "guest") {
+      dispatch(
+        createNotification({
+          message: "Authentication required",
+          isSuccess: false,
+        })
+      );
+      navigate("/login");
+    } else {
+      dispatch(addOrder(cartState));
+    }
+  };
+
+  if (!cartState || cartState.length === 0) {
+    return (
+      <div data-testid="cart-component">
+        <div data-testid="empty-cart"></div>
+      </div>
+    );
+  }
+
+  const cartItems = cartState.map((val) => CartItem(val));
+
+  return (
+    <div data-testid="cart-component">
+      <div data-testid="cart-item-container">{cartItems}</div>
+      <button data-testid="order-button" onClick={orderClick}></button>
+    </div>
+  );
+};
 
 export default Cart;
--- assignment/frontend/src/components/CartItem.jsx
+++ assignment/frontend/src/components/CartItem.jsx
@@ -1,13 +1,62 @@
 /** @format */
 
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useDispatch } from "react-redux";
 import {
-	decrementCartItem,
-	incrementCartItem,
-	removeCartItem,
-} from '../redux/actionCreators/cartActions';
+  decrementCartItem,
+  incrementCartItem,
+  removeCartItem,
+} from "../redux/actionCreators/cartActions";
 
-const CartItem = ({ item }) => {};
+/**
+ * Render cart item
+ * @component
+ */
+const CartItem = ({ product, quantity }) => {
+  const dispatch = useDispatch();
+
+  /**
+   * Decrement or remove item from cart
+   * @param {*} e Click event
+   */
+  const decrementQuantity = (e) => {
+    e.preventDefault();
+
+    if (quantity > 1) {
+      dispatch(decrementCartItem(product.id));
+    } else {
+      dispatch(removeCartItem(product));
+    }
+  };
+
+  /**
+   * Increment item amount in cart
+   * @param {*} e Event
+   */
+  const incrementQuantity = (e) => {
+    e.preventDefault();
+    dispatch(incrementCartItem(product.id));
+  };
+
+  return (
+    <div data-testid="cart-item-component" key={`cart-item-${product.id}`}>
+      <p data-testid="item-name">{product.name}</p>
+      <p data-testid="item-price">{product.price}</p>
+      <p data-testid="item-amount">{quantity}</p>
+      <button
+        data-testid={`plus-btn-${product.id}`}
+        onClick={incrementQuantity}
+      >
+        (+)
+      </button>
+      <button
+        data-testid={`minus-btn-${product.id}`}
+        onClick={decrementQuantity}
+      >
+        (-)
+      </button>
+    </div>
+  );
+};
 
 export default CartItem;
