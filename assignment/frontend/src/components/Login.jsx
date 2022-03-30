ecd3a8995b4827afb7b6b956ce8ed6ec85053c7a
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-30 21:57:36 +0300

    Implement Login

--- assignment/frontend/src/components/Login.jsx
+++ assignment/frontend/src/components/Login.jsx
@@ -1,9 +1,48 @@
 /** @format */
 
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { logIn } from '../redux/actionCreators/authActions';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logIn } from "../redux/actionCreators/authActions";
 
-const Login = () => {};
+const Login = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(logIn({ email, password }));
+  };
+
+  return (
+    <div data-testid="login-component" onSubmit={onSubmit}>
+      <form data-testid="login-form">
+        <label>
+          Email:
+          <input
+            data-testid="email-input"
+            type="email"
+            placeholder="user@example.com"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            data-testid="password-input"
+            type="password"
+            placeholder="password (at least 10 characters"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <input data-testid="login-button" type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 
 export default Login;
