df100ac8f267ffa2227831b722552a15f8b1840f
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-31 00:34:26 +0300

    Implement Modify

--- assignment/frontend/src/components/User.jsx
+++ assignment/frontend/src/components/User.jsx
@@ -1,10 +1,68 @@
 /** @format */
 
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { removeUser } from '../redux/actionCreators/usersActions';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { removeUser } from "../redux/actionCreators/usersActions";
 
-const User = ({ providedUser }) => {};
+const User = ({ providedUser }) => {
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
+  const auth = useSelector((state) => state.auth);
+
+  let user = providedUser;
+
+  if (userId) {
+    user = users.find((val) => val.id === userId);
+  }
+
+  const { id, name, email, role } = user;
+  const canRemove =
+    auth && auth.role && auth.role === "admin" && auth.id !== id;
+
+  /**
+   * Delete user
+   */
+  const deleteUser = () => {
+    dispatch(removeUser(id));
+  };
+
+  /**
+   * Navigate to user modift
+   */
+  const navigateToModify = () => {
+    if (userId) {
+      navigate("modify");
+    } else {
+      navigate(`${id}/modify`);
+    }
+  };
+
+  return (
+    <div data-testid="user-component">
+      <h3 data-testid="name-heading">{name}</h3>
+      {!userId && (
+        <Link data-testid="inspect-link" to={`/${id}`}>
+          Inspect
+        </Link>
+      )}
+      <p data-testid="email-element">{email}</p>
+      <p data-testid="role-element">{role}</p>
+      {canRemove && (
+        <button data-testid={`modify-button-${id}`} onClick={navigateToModify}>
+          Modify
+        </button>
+      )}
+      {canRemove && (
+        <button data-testid={`delete-button-${id}`} onClick={deleteUser}>
+          Delete
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default User;
