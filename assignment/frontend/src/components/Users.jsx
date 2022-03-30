9987f5ec99dd52164abc85194a26c299e9fbd2fb
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-31 01:05:16 +0300

    Implement Users

--- assignment/frontend/src/components/Users.jsx
+++ assignment/frontend/src/components/Users.jsx
@@ -1,10 +1,29 @@
 /** @format */
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUsers } from '../redux/actionCreators/usersActions';
-import User from './User';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../redux/actionCreators/usersActions";
+import User from "./User";
 
-const Users = () => {};
+const Users = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
+
+  useEffect(() => {
+    if (!users || users.length === 0) {
+      dispatch(getUsers());
+    }
+  });
+
+  const userList = users.map((val) => (
+    <User providedUser={val} key={`user-${val.id}`} />
+  ));
+
+  return (
+    <div data-testid="users-component">
+      <ul data-testid="users-container">{userList}</ul>
+    </div>
+  );
+};
 
 export default Users;
