779f849bbc0c6037dd8647dad5d885be913958de
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-31 00:09:23 +0300

    Implement Products

--- assignment/frontend/src/components/Products.jsx
+++ assignment/frontend/src/components/Products.jsx
@@ -1,11 +1,58 @@
 /** @format */
 
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProducts } from '../redux/actionCreators/productsActions';
-import Product from './Product';
-import ProductAdder from './ProductAdder';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../redux/actionCreators/productsActions";
+import Product from "./Product";
+import ProductAdder from "./ProductAdder";
 
-const Products = () => {};
+const Products = () => {
+  const dispatch = useDispatch();
+  const [adderOpen, setAdderOpen] = useState(false);
+
+  const products = useSelector((state) => state.products);
+  const role = useSelector((state) => state.auth).role;
+  const isAdmin = role && role === "admin";
+
+  useEffect(() => {
+    if (!products || products.length === 0) {
+      dispatch(getProducts());
+    }
+  });
+
+  /**
+   * Handles clicks of open adder
+   * @param {*} e Event
+   */
+  const openAdderClick = (e) => {
+    e.preventDefault();
+    setAdderOpen(!adderOpen);
+  };
+
+  /**
+   * Close adder
+   */
+  const closeAdder = () => {
+    setAdderOpen(false);
+  };
+
+  const productList = products.map((val) => (
+    <Product providedProduct={val} key={`product-${val.id}`} />
+  ));
+
+  return (
+    <div data-testid="products-component">
+      {isAdmin && (
+        <button data-testid="open-adder-button" onClick={openAdderClick}>
+          {(adderOpen && "Close") || "Open"}
+        </button>
+      )}
+      {isAdmin && adderOpen && (
+        <ProductAdder open={adderOpen} openHandler={closeAdder} />
+      )}
+      <ul data-testid="products-container">{productList}</ul>
+    </div>
+  );
+};
 
 export default Products;
