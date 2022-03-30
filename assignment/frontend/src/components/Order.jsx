fa3b96e8369a1cd8f45385e58ee14c08e0034e99
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-03-30 22:52:41 +0300

    Implement order and orders

--- assignment/frontend/src/components/Order.jsx
+++ assignment/frontend/src/components/Order.jsx
@@ -1,9 +1,44 @@
 /** @format */
 
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
 
-const Order = ({ providedOrder }) => {};
+const Order = ({ providedOrder }) => {
+  const { orderId } = useParams();
+  const orders = useSelector((state) => state.orders);
+
+  let order = providedOrder;
+
+  if (orderId) {
+    order = orders.find((val) => val.id === orderId);
+  }
+
+  const { id, customerId, items } = order;
+
+  const orderList = items.map(({ product, quantity }) => {
+    return (
+      <li data-testid="order-listitem" key={`listitem-${product.id}`}>
+        <h3 data-testid="name-heading">{product.name}</h3>
+        <p data-testid="price-element">{product.price}</p>
+        <p data-testid="description-element">{product.description}</p>
+        <p data-testid="quantity-element">{quantity}</p>
+      </li>
+    );
+  });
+
+  return (
+    <div data-testid="order-component">
+      <h3 data-testid="orderId-heading">{id}</h3>
+      <h3 data-testid="customerId-heading">{customerId}</h3>
+      {!orderId && (
+        <Link data-testid="inspect-link" to={`/${id}`}>
+          Inspect
+        </Link>
+      )}
+      <ol data-testid="order-list">{orderList}</ol>
+    </div>
+  );
+};
 
 export default Order;
--- assignment/frontend/src/components/Orders.jsx
+++ assignment/frontend/src/components/Orders.jsx
@@ -1,10 +1,43 @@
 /** @format */
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getOrders } from '../redux/actionCreators/ordersActions';
-import Order from './Order';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getOrders } from "../redux/actionCreators/ordersActions";
+import Order from "./Order";
 
-const Orders = () => {};
+const Orders = () => {
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.orders);
+
+  useEffect(() => {
+    if (!orders || orders.length === 0) {
+      dispatch(getOrders());
+    }
+  }, [orders, dispatch]);
+
+  if (!orders || orders.length === 0) {
+    return <div data-testid="no-order-component"></div>;
+  }
+
+  const orderList = orders.map((val) => {
+    return (
+      <Order
+        providedOrder={{
+          id: val.id,
+          customerId: val.customerId,
+          items: val.items,
+        }}
+        data-testid="order-component"
+        key={`order-component-${val.id}`}
+      />
+    );
+  });
+
+  return (
+    <div data-testid="orders-component">
+      <ul data-testid="orders-container">{orderList}</ul>
+    </div>
+  );
+};
 
 export default Orders;
